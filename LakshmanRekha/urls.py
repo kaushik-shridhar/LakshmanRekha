9b23954d2f23572d80b29e3c2009c214b41f21f9
Kaushik Shridhar <shridharkaushik1@gmail.com>
2020-12-07 11:54:53 +0530

    created canvas for geofence creation

--- LakshmanRekha/urls.py
+++ LakshmanRekha/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('login/', include('login.urls')),
+    path('', include('login.urls')),
+    path('canvas/', include('geofence.urls')),
     path('admin/', admin.site.urls),
 ]
--- login/views.py
+++ login/views.py
@@ -2,5 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+# home
 def index(request):
-    return HttpResponse("Hello, World!")
+    return render(request, 'login/index.html')
+
+# registration
+def register(request):
+    if (request.method == 'POST'):
+        first_name = request.POST['first_name']
+        print(first_name)
+        return render(request, 'login/index.html')
+    else:
+        return render(request, 'login/register.html')
+
+# login
+def login(request):
+    if (request.method == 'POST'):
+        username = request.POST['username']
+        return render(request, 'login/index.html')
+    else:
+        return render(request, 'login/login.html')
